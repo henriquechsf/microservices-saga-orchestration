3c13cdaeab42f42410cee7e17742f58917dbbeb9
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-01-11 01:33:26 -0300

    25. json util and exceptions

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/utils/JsonUtil.java
@@ -0,0 +1,33 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.utils;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper mapper;
+
+    public String toJson(Object object) {
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            return "";
+        }
+    }
+
+    public Event toEvent(String json) {
+        try {
+            return mapper.readValue(json, Event.class);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            return null;
+        }
+    }
+}
