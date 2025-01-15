da30ebfd663c07ce266c91656f2c1e666e35d7b8
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-01-14 22:17:20 -0300

    34. product validation service - insert initial data

---
+++ product-validation-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into product (id, code) values (1, 'COMIC BOOKS');
+insert into product (id, code) values (2, 'BOOKS');
+insert into product (id, code) values (3, 'MOVIES');
+insert into product (id, code) values (4, 'MUSIC');
