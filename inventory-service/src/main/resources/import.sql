437fbaf5367da202f8d9631ad804ec8fe9575dc5
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-02-06 21:40:17 -0300

    45. inventory service - initial data

---
+++ inventory-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into inventory (id, product_code, available) values (1, 'COMIC BOOKS', 4);
+insert into inventory (id, product_code, available) values (2, 'BOOKS', 2);
+insert into inventory (id, product_code, available) values (3, 'MOVIES', 5);
+insert into inventory (id, product_code, available) values (4, 'MUSIC', 9);
